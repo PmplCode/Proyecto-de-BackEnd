c28e1a4679e30b7ea573a63956f5e01f3bf2b831
Eloi Pampliega <eloipampliega@gmail.com>
2022-10-17 19:23:18 +0200

    login / register funcional

--- cocteleria/routes/auth.routes.js
+++ cocteleria/routes/auth.routes.js
@@ -61,10 +61,10 @@ router.post("/signup", isLoggedOut, (req, res) => {
     .then((salt) => bcrypt.hash(password, salt))
     .then((hashedPassword) => {
       // Create a user and save it in the database
-      return User.create({ username, email, password: hashedPassword });
+      return User.create({ username, password: hashedPassword });
     })
     .then((user) => {
-      res.redirect("/");
+      res.redirect("/profile");
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
@@ -85,12 +85,12 @@ router.get("/", isLoggedOut, (req, res) => {
   res.render("index");
 });
 
-// POST /auth/login
+// POST /auth/
 router.post("/", isLoggedOut, (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, password } = req.body;
 
   // Check that username, email, and password are provided
-  if (username === "" || email === "" || password === "") {
+  if (username === "" || password === "") {
     res.status(400).render("index", {
       errorMessage:
         "All fields are mandatory. Please provide username, email and password.",
@@ -108,7 +108,7 @@ router.post("/", isLoggedOut, (req, res, next) => {
   }
 
   // Search the database for a user with the email submitted in the form
-  User.findOne({ email })
+  User.findOne({ username })
     .then((user) => {
       // If the user isn't found, send an error message that user provided wrong credentials
       if (!user) {
@@ -134,7 +134,7 @@ router.post("/", isLoggedOut, (req, res, next) => {
           // Remove the password field
           delete req.session.currentUser.password;
 
-          res.redirect("/");
+          res.redirect("/profile");
         })
         .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
     })
