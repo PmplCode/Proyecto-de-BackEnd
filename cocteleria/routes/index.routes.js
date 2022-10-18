990bf527d511e788890f98c3c386d2ebce4aa38b
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-10-18 14:15:15 +0200

    cloudinary hecho

--- cocteleria/routes/index.routes.js
+++ cocteleria/routes/index.routes.js
@@ -6,12 +6,21 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 const User = require("../models/User.model");
 const Coctel = require("../models/Coctel.model");
+const fileUploader = require('../config/cloudinary.config');
+
+const path = require("path");
+
 
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
 
 router.get("/principal", (req, res, next) => {
   Coctel.find()
@@ -23,22 +32,25 @@ router.get("/principal", (req, res, next) => {
     console.log("error render principal: ", err)
   })
 });
-
-router.get("/profile", (req, res, next) => {
-  res.render("profile");
-});
-
 router.get("/crear", (req, res, next) => {
   res.render("crear")
 })
 
-router.post("/crear", (req, res, next) => {
-  const { name, alcohol, ingredientes, pais, descripcion, procedimiento } = req.body;
 
-  Coctel.create({ name, alcohol, ingredientes, pais, descripcion, procedimiento })
-  .then(resp => {
+router.post("/crear",fileUploader.single('coctel-cover-image'), (req, res) => {
+  const { name, alcohol, ingredientes } = req.body;
+    console.log("req.file", req.file);
+
+
+  Coctel.create({ name, alcohol, ingredientes, imageUrl: req.file.path })
+    .then(newlyCreatedCoctelFromDB => {
+        console.log("newlyCreatedCoctelFromDB: ", newlyCreatedCoctelFromDB);
+    
     res.redirect("/principal")
   })
+  .catch(error => console.log(`Error while creating a new coctel: ${error}`));
 })
+
+
 
 module.exports = router;
