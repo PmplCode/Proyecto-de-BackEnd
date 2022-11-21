cc094810d838c9b6851103885172e4b62749d547
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-11-21 21:05:38 +0100

    arreglos

--- cocteleria/routes/index.routes.js
+++ cocteleria/routes/index.routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const capitalize = require("../utils/capitalize");
@@ -7,154 +7,154 @@ const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isPremium = require("../middleware/isPremium");
 
-
 const User = require("../models/User.model");
 const Coctel = require("../models/Coctel.model");
-const fileUploader = require('../config/cloudinary.config');
+const fileUploader = require("../config/cloudinary.config");
 
 const path = require("path");
-const { handlebars } = require('hbs');
-
-
+const { handlebars } = require("hbs");
 
 /* GET home page */
 router.get("/", isLoggedOut, (req, res, next) => {
   res.render("index");
 });
 
-
 router.get("/principal", isLoggedIn, (req, res, next) => {
-  const userId = req.params.idUser
+  const userId = req.params.idUser;
 
   Coctel.find()
-  .then(resp => {
-    const data = { 
-      coctel: resp,
-      usuario: req.session.currentUser
-    }
-   
-    console.log("req.session.currentUser - principal: ", req.session.currentUser)
-    res.render("principal", data);
-  })
-  .catch(err => {
-    console.log("error render principal: ", err)
-  })
+    .then((resp) => {
+      const data = {
+        coctel: resp,
+        usuario: req.session.currentUser,
+      };
+
+      res.render("principal", data);
+    })
+    .catch((err) => {
+      console.log("error render principal: ", err);
+    });
 });
 
-router.get("/premium", isLoggedIn, isPremium, (req,res,next) => {
+router.get("/premium", isLoggedIn, isPremium, (req, res, next) => {
   const key = {
-    key1: process.env.MAPS
-  }
-  console.log("key; ", key)
-  res.render("./auth/premium", key)
-})
+    key1: process.env.MAPS,
+  };
+  res.render("./auth/premium", key);
+});
 
 router.get("/editarPerfil", (req, res, next) => {
-  const data = { 
-    usuario: req.session.currentUser
-  }
-  res.render("editarPerfil", data)
-})
-
-router.post("/editarPerfil", isLoggedIn, fileUploader.single("perfil-cover-image"), (req, res, next) => {
-  const { username, descripcion } = req.body;
-  
-  req.session.currentUser.username = req.body.username
-  req.session.currentUser.descripcion = req.body.descripcion
-  req.session.currentUser.fotoPerfil = req.file.path
-  
-  User.findByIdAndUpdate(req.session.currentUser._id,{username, descripcion, fotoPerfil:req.file.path})
-  .then(userEditedFromDB => {
-    console.log("userEditedFromDB: ", userEditedFromDB);
-    
-    res.redirect("/profile/"+req.session.currentUser._id)
-  })
-  .catch(err=>{
-    console.log("error actualitzar foto perfil: ",err)
-  })
-})
-
-
-router.get("/crear", isLoggedIn, (req, res, next) => {
-  const data = { 
-    usuario: req.session.currentUser
-  }
-  res.render("crear", data)
-})
-
-
-router.post("/crear", isLoggedIn, fileUploader.single('coctel-cover-image'), (req, res) => {
-  const { name, alcohol, ingredientes, descripcion, origen } = req.body;
-  console.log("req.body", req.body);
-  // const elAlcohol = capitalize(alcohol)
-  
-  let paraules = [];
-  
-  alcohol.forEach((paraula, k) => {
-    if(paraula.length > 0)
-    paraules.push(capitalize(paraula))
-  })
-  console.log("paraules: ", paraules)
-  Coctel.create({ name, alcohol: paraules, ingredientes, descripcion,origen, imageUrl: req.file.path , creador: req.session.currentUser._id})
-  .then(newlyCreatedCoctelFromDB => {
-    console.log("newlyCreatedCoctelFromDB: ", newlyCreatedCoctelFromDB);
-    
-    res.redirect("/principal")
-  })
+  const data = {
+    usuario: req.session.currentUser,
+  };
+  res.render("editarPerfil", data);
 });
 
+router.post(
+  "/editarPerfil",
+  isLoggedIn,
+  fileUploader.single("perfil-cover-image"),
+  (req, res, next) => {
+    const { username, descripcion } = req.body;
+
+    req.session.currentUser.username = req.body.username;
+    req.session.currentUser.descripcion = req.body.descripcion;
+    req.session.currentUser.fotoPerfil = req.file.path;
+
+    User.findByIdAndUpdate(req.session.currentUser._id, {
+      username,
+      descripcion,
+      fotoPerfil: req.file.path,
+    })
+      .then((userEditedFromDB) => {
+        res.redirect("/profile/" + req.session.currentUser._id);
+      })
+      .catch((err) => {
+        console.log("error actualitzar foto perfil: ", err);
+      });
+  }
+);
+
+router.get("/crear", isLoggedIn, (req, res, next) => {
+  const data = {
+    usuario: req.session.currentUser,
+  };
+  res.render("crear", data);
+});
+
+router.post(
+  "/crear",
+  isLoggedIn,
+  fileUploader.single("coctel-cover-image"),
+  (req, res) => {
+    const { name, alcohol, ingredientes, descripcion, origen } = req.body;
+    // const elAlcohol = capitalize(alcohol)
+
+    let paraules = [];
+
+    alcohol.forEach((paraula, k) => {
+      if (paraula.length > 0) paraules.push(capitalize(paraula));
+    });
+    Coctel.create({
+      name,
+      alcohol: paraules,
+      ingredientes,
+      descripcion,
+      origen,
+      imageUrl: req.file.path,
+      creador: req.session.currentUser._id,
+    }).then((newlyCreatedCoctelFromDB) => {
+      res.redirect("/principal");
+    });
+  }
+);
+
 router.get("/search", isLoggedIn, (req, res, next) => {
-  let paraula1 = req.query.alcohol
-  if(paraula1.trim().length === 0){
-    return res.redirect("/principal")
+  let paraula1 = req.query.alcohol;
+  if (paraula1.trim().length === 0) {
+    return res.redirect("/principal");
   } else {
-  console.log("req.query search: ", req.query.alcohol)
-  
-  const paraula = capitalize(req.query.alcohol)
-  
-  const filtre = { alcohol: {$regex : `${paraula}`}};
-  console.log("filtre regex: ", filtre)
-  
-  Coctel.find(filtre)
-  .then(resp => {
-    const data = { 
-      coctel: resp,
-      usuario: req.session.currentUser,
-      palab: req.query
-    }
-    res.render("principal", data);
-  })}
-})
+    const paraula = capitalize(req.query.alcohol);
+
+    const filtre = { alcohol: { $regex: `${paraula}` } };
+
+    Coctel.find(filtre).then((resp) => {
+      const data = {
+        coctel: resp,
+        usuario: req.session.currentUser,
+        palab: req.query,
+      };
+      res.render("principal", data);
+    });
+  }
+});
 
 router.get("/logout", isLoggedIn, (req, res, next) => {
   req.session.destroy();
-  res.redirect("/")
-})
+  res.redirect("/");
+});
 
 router.get("/profile/:idUser", isLoggedIn, (req, res, next) => {
-  const user = req.session.currentUser
-  const userId = req.params.idUser
+  const user = req.session.currentUser;
+  const userId = req.params.idUser;
   const userImg = req.session.currentUser.fotoPerfil;
 
-  console.log("userId: ", userId)
-  
-  console.log("USERRRRR :",req.session.currentUser)
   Coctel.find({ creador: userId })
     .populate("creador")
-    .then(results => {
+    .then((results) => {
       const data = {
         coctelUser: results,
-        usuario: req.session.currentUser
-      }
-      console.log("results profile: ", results)
-      
+        usuario: req.session.currentUser,
+      };
+
       if (req.session.currentUser._id == userId) {
-        data.user = req.session.currentUser
-      }else {data.user = results[0].creador}
+        data.user = req.session.currentUser;
+      } else {
+        data.user = results[0].creador;
+      }
 
-    res.render("profile", data);
-  })
-
+      res.render("profile", data);
+    });
 });
 
 //NO FA RES, PEL QUE SEMBLA, ESPERAR A BORRAR!!!!
@@ -171,90 +171,94 @@ router.get("/profile/:idUser", isLoggedIn, (req, res, next) => {
 //   })
 // })
 
-
 router.get("/informacion/:coctelId/edit", isLoggedIn, (req, res, next) => {
   Coctel.findById(req.params.coctelId)
     .populate("creador")
-    .then(result => {
-      console.log("resultado entar editar: ", result)
-      const data = {
-      coctel: result
-    }
-    res.render("editCoctel", data)
-  })
-})
-
-router.post("/informacion/:coctelId/edit", isLoggedIn, (req, res, next) => {
-  const coctelId = req.params.coctelId
-  Coctel.findById(coctelId)
-    .populate("creador")
-    .then(result => {
-    let whatever = `${result.creador._id}`
-    if(whatever.includes(req.session.currentUser._id)){
-    console.log("req.body edit: ", req.body)
-
-    const { name, alcohol, ingredientes, procedimiento, descripcion, origen } = req.body;
-  
-    let paraules = [];
-
-    alcohol.forEach((paraula) => {
-      if(paraula.length > 0)
-      paraules.push(capitalize(paraula))
-    })
-
-    Coctel.findByIdAndUpdate(req.params.coctelId, {name, alcohol: paraules, ingredientes, procedimiento, descripcion, origen}, {new: true})
-    .then(resp => {
-      return res.redirect("/informacion/"+req.params.coctelId)
-    })
-    .catch(err => {
-      return console.log("err: ", err)
-    })
-    } else {
-    return res.redirect("/principal")
-  }})
-})
-
-router.get("/informacion/:coctelId", isLoggedIn, (req, res, next) => {
-  const coctelId = req.params.coctelId
-  Coctel.findById(coctelId)
-    .populate("creador")
-    .then(result => {
-      console.log("result ", result)
-      console.log("req.session.currentUser._id ", req.session.currentUser._id)
-      console.log("result.creador._id ", result.creador._id)
-
-
+    .then((result) => {
       const data = {
         coctel: result,
-        usuario: req.session.currentUser
+      };
+      res.render("editCoctel", data);
+    });
+});
+
+router.post("/informacion/:coctelId/edit", isLoggedIn, (req, res, next) => {
+  const coctelId = req.params.coctelId;
+  Coctel.findById(coctelId)
+    .populate("creador")
+    .then((result) => {
+      let whatever = `${result.creador._id}`;
+      if (whatever.includes(req.session.currentUser._id)) {
+        const {
+          name,
+          alcohol,
+          ingredientes,
+          procedimiento,
+          descripcion,
+          origen,
+        } = req.body;
+
+        let paraules = [];
+
+        alcohol.forEach((paraula) => {
+          if (paraula.length > 0) paraules.push(capitalize(paraula));
+        });
+
+        Coctel.findByIdAndUpdate(
+          req.params.coctelId,
+          {
+            name,
+            alcohol: paraules,
+            ingredientes,
+            procedimiento,
+            descripcion,
+            origen,
+          },
+          { new: true }
+        )
+          .then((resp) => {
+            return res.redirect("/informacion/" + req.params.coctelId);
+          })
+          .catch((err) => {
+            return console.log("err: ", err);
+          });
+      } else {
+        return res.redirect("/principal");
       }
+    });
+});
+
+router.get("/informacion/:coctelId", isLoggedIn, (req, res, next) => {
+  const coctelId = req.params.coctelId;
+  Coctel.findById(coctelId)
+    .populate("creador")
+    .then((result) => {
+      const data = {
+        coctel: result,
+        usuario: req.session.currentUser,
+      };
 
       // if(`new ObjectId("${req.session.currentUser._id}")` == `result.creador._id`) {
       //   data.esCreador = true;
       // }
-      let whatever = `${result.creador._id}`
-      if(whatever.includes(req.session.currentUser._id)){
+      let whatever = `${result.creador._id}`;
+      if (whatever.includes(req.session.currentUser._id)) {
         data.esCreador = true;
       }
 
-    res.render("informacion", data)
-  })
-  .catch(err => {
-    console.log("err: ", err)
-  })
-})
+      res.render("informacion", data);
+    })
+    .catch((err) => {
+      console.log("err: ", err);
+    });
+});
 
-
-
-  router.post("/delete/:coctelId", isLoggedIn, (req,res)=>{
-  console.log("req.params.coctelId: ", req.params.coctelId)
-
+router.post("/delete/:coctelId", isLoggedIn, (req, res) => {
   Coctel.findByIdAndDelete(req.params.coctelId)
-   .then(() => {
-    res.redirect("/profile/" + req.session.currentUser._id)
-   })
-   .catch((error) => console.log(error));
- })
-  
-  module.exports = router;
+    .then(() => {
+      res.redirect("/profile/" + req.session.currentUser._id);
+    })
+    .catch((error) => console.log(error));
+});
 
+module.exports = router;
